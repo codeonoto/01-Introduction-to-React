5f8067f0147319e0c242e9a84a3046a190ce1a65
codeonoto <onoto0409@gmail.com>
2024-03-04 18:44:51 +0530

    Components using functions

--- index.js
+++ index.js
@@ -12,5 +12,30 @@
 // ReactDOM.createRoot(document.getElementById('root')).render(reactImage);
 
 // <-- React With JSX -->
-const jsxHeading = <h1>Hello JSX !</h1>;
-ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
+// const jsxHeading = (
+//   <>
+//     <h1>About React</h1>
+//     <p>There are some features of React</p>
+//     <ul>
+//       <li>React is Compasable.</li>
+//       <li>React have better Code Reusability.</li>
+//       <li>React is Declareative.</li>
+//     </ul>
+//   </>
+// );
+
+function App() {
+  return (
+    <>
+      <h1>About React</h1>
+      <p>There are some features of React</p>
+      <ul>
+        <li>React is Compasable.</li>
+        <li>React have better Code Reusability.</li>
+        <li>React is Declareative.</li>
+      </ul>
+    </>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
