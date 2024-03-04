e4a841a61918141539926a857b7701929870c8f2
codeonoto <onoto0409@gmail.com>
2024-03-04 18:22:29 +0530

    Understanding BABEL

--- index.js
+++ index.js
@@ -1,11 +1,16 @@
-const reactHeading = React.createElement('h1', {
-  className: 'head',
-  children: 'Hello React !',
-});
-const reactImage = React.createElement('img', {
-  src: 'https://files.codingninjas.in/coding-ninjas-24647.png',
-  alt: 'Coding Ninjas Logo',
-});
+// <-- React With JS -->
+// const reactHeading = React.createElement('h1', {
+//   className: 'head',
+//   children: 'Hello React !',
+// });
+// const reactImage = React.createElement('img', {
+//   src: 'https://files.codingninjas.in/coding-ninjas-24647.png',
+//   alt: 'Coding Ninjas Logo',
+// });
 
 // ReactDOM.createRoot(document.getElementById('root')).render(reactHeading);
-ReactDOM.createRoot(document.getElementById('root')).render(reactImage);
+// ReactDOM.createRoot(document.getElementById('root')).render(reactImage);
+
+// <-- React With JSX -->
+const jsxHeading = <h1>Hello JSX !</h1>;
+ReactDOM.createRoot(document.getElementById('root')).render(jsxHeading);
